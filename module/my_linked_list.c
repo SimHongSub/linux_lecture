03d1d0cdd685d68780580c9008324c5b843c206c
SimHongsub <oxhe2038@naver.com>
2020-12-07 08:22:35 +0900

    implement delete function

--- module/my_linked_list.c
+++ module/my_linked_list.c
@@ -10,8 +10,10 @@
 
 long long insert_time[2];
 long long search_time[2];
+long long delete_time[2];
 
-int search_time_index = 0;
+int search_time_index = -1;
+int delete_time_index = -1;
 
 struct my_node{
 	struct list_head entry;
@@ -41,8 +43,6 @@ int insert_thread(void *data){
 
 	temp->data = size;
 
-	//__sync_fetch_and_add(&insert_time, (long long)ktime_sub(end, start));
-
 	return 0;
 }
 
@@ -62,7 +62,6 @@ void my_insert(struct my_node **_arg, int index){
 }
 
 int search_thread(void* data){
-	int index = search_time_index;
 	struct my_node *current_node;
 	struct my_node* temp = (struct my_node*)data;
 	ktime_t start, end;
@@ -70,13 +69,14 @@ int search_thread(void* data){
 	start = ktime_get_ns();
         list_for_each_entry(current_node, &(temp->entry), entry){
 		if(current_node->data == temp->find_data){
-                	printk("%d\n", current_node->data);
+                	printk("search data : %d\n", current_node->data);
 			break;
 		}
         }
 	end = ktime_get_ns();
 
-	search_time[index] = (long long)ktime_sub(end, start);
+	search_time_index++;
+	search_time[search_time_index] = (long long)ktime_sub(end, start);
 
 	return 0;
 
@@ -88,22 +88,47 @@ void my_search(struct my_node **_arg, int data){
 	for(i=0;i<2;i++){
 		_arg[i]->find_data = data;
 		kthread_run(search_thread, (void*)_arg[i], "search_thread");
-		search_time_index++;
+	}
+}
+
+int delete_thread(void* data){
+	struct my_node *current_node;
+	struct my_node *tmp;
+	struct my_node *temp = (struct my_node*)data;
+	ktime_t start, end;
+
+	start = ktime_get_ns();
+	list_for_each_entry_safe(current_node, tmp, &(temp->entry), entry){
+		if(current_node->data == temp->find_data){
+			printk("delete data : %d\n", current_node->data);
+			list_del(&current_node->entry);
+			kfree(current_node);
+			temp->data--;
+		}
+	}
+	end = ktime_get_ns();
+
+	delete_time_index++;
+	delete_time[delete_time_index] = (long long)ktime_sub(end, start);
+
+	return 0;
+}
+
+void my_delete(struct my_node **_arg, int data){
+	int i;
+
+	for(i=0;i<2;i++){
+		_arg[i]->find_data = data;
+		kthread_run(delete_thread, (void*)_arg[i], "delete_thread");
 	}
 }
 
 void example(void){
 	struct my_node* lists[2];
-	struct my_node *current_node;
 
 	my_insert(lists, 10000);
 
 	msleep(10000);
-
-	/*list_for_each_entry(current_node, &(lists[0]->entry), entry){
-		
-		printk("%d\n", current_node->data);
-	}*/
 
 	if(insert_time[0] < insert_time[1]){
 		printk("insert time : %lld ns\n", insert_time[1]);
@@ -111,18 +136,33 @@ void example(void){
 		printk("insert time : %lld ns\n", insert_time[0]);
 	}
 
-	my_search(lists, 6000);
+	printk("---------SEARCH---------");
+
+	my_search(lists, 9000);
 
 	msleep(10000);
 
 	if(search_time[0] < search_time[1]){
-                printk("search time : %lld ns\n", search_time[1]);
-        }else{
                 printk("search time : %lld ns\n", search_time[0]);
+        }else{
+                printk("search time : %lld ns\n", search_time[1]);
         }
 
-	//printk("size : %d\n", lists[0]->data);
+	printk("Data size : %d\n", lists[0]->data + lists[1]->data);
 
+	printk("---------DELETE---------");
+
+	my_delete(lists, 4000);
+
+	msleep(10000);
+
+	if(delete_time[0] < delete_time[1]){
+                printk("delete time : %lld ns\n", delete_time[1]);
+        }else{
+                printk("delete time : %lld ns\n", delete_time[0]);
+        }
+
+	printk("Data size : %d\n", lists[0]->data + lists[1]->data);
 }
 
 int __init my_linked_list_init(void){
